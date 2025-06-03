a542815c8bcfc86c56e24395b922b6be9fce2dcc
VinkoRobi2 <vinkolino1@gmail.com>
2025-06-03 06:52:01 -0500

    Arreglos Backen, no avances

--- Ingefitness/src/backend/main.go
+++ Ingefitness/src/backend/main.go
@@ -1,61 +1,63 @@
 package main
 
 import(
-	"log"
 	"net/http"
 		"database/sql"
 	_ "github.com/go-sql-driver/mysql" 
 	"github.com/gin-gonic/gin"
 )
+var db *sql.DB
 
 type User struct {
-	Nombre string "nombre:json"
-	Apellido string "nombre:json"
-	Email string "email:json"
-	fecha int 4
+    Nombre   string `json:"nombre"`
+    Apellido string `json:"apellido"`
+    Email    string `json:"email"`
+    Fecha    int    `json:"fecha"`
 }
-type Admin struct{
-	Username string "username:json"
-	Password string "password:json"
+
+type Admin struct {
+    Username string `json:"username"`
+    Password string `json:"password"`
 }
-func Login(c*gin.context){
+
+func Login(c*gin.Context){
 	var admin Admin
-	if err := c.BindJson(admin);err!= nil{
-		c.JSON(http.StatusBadrequest{
+	if err := c.BindJSON(admin);err!= nil{
+		c.JSON(http.StatusBadRequest, gin.H{
 			"message":"Datos enviados erroneamente",
 			"success":false,
 		},)
 	}
 	var storedpass string
-	err := db.QueryRow("SELECT password FROM usuarios WHERE username = ?", admin.Admin).Scan(&storedpass)
+	err := db.QueryRow("SELECT password FROM usuarios WHERE username = ?", admin.Username).Scan(&storedpass)
 	if err != nil {
 		if err == sql.ErrNoRows	{
 			c.JSON(http.StatusBadRequest,gin.H{
 				"message":"Usurio incorrecto o no existe",
 				"sucess": false,
-			}
+			},
 			)
 			return
 		}
-		c.JSON(http.StatusInternalserverError,gin.H{
+		c.JSON(http.StatusInternalServerError,gin.H{
 			"message":"Error Interno del servidor",
-			"success":false
-		}
+			"success":false,
+		},
 		)
 		return
 	}
-	if storedpass != admin.password{
-		c.JSON(http.Statusunathorized,gin.H{
+	if storedpass != admin.Password{
+		c.JSON(http.StatusUnauthorized,gin.H{
 			"message":"Contrasena incorrecta",
 			"success":false,
-		}
+		},
 		)
 		return
 	}
 	c.JSON(http.StatusAccepted, gin.H{
-		"message":"Inicio de sesion exitoso"
-		"success":true
-	}
+		"message":"Inicio de sesion exitoso",
+		"success":true,
+	},
 	)
 }
 
