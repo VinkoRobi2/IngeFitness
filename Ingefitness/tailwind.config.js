81831aeb3f4aa39bd3ac77b1ccc80cdd95c0a070
Ricardo <122332008+ricardopugar@users.noreply.github.com>
2025-06-06 12:22:18 -0500

    Create tailwind.config.js

---
+++ Ingefitness/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // ⬅️ Aquí le dices a Tailwind que revise todos tus componentes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
